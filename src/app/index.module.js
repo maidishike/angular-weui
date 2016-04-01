8696b94da28a91e4ec5f70af888a561f2b5cf71c
maidishike <wsgtracy@163.com>
2016-04-01 09:58:00 +0800

    定制路由规则

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,7 +2,5 @@
   'use strict';
 
   angular
-    .module('app', ['ui.router', 'oc.lazyLoad',  'w5c.validator','app.pagination',
-      'ngFileUpload','ngjsColorPicker','cnCitySelect',
-      'app.highcharts','app.particles','app.tm.pagination','app.alert.modal','focus-if',"ngProgress"]);
+    .module('app', ['ui.router', 'focus-if',"ngProgress"]);
 })();
